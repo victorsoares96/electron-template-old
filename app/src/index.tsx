7d629b1c1f9b484875f4f936f147ae1c37b6e739
Victor Soares <vitorsoares96@hotmail.com>
2023-01-15 19:51:47 -0300

    :beers: gibberish:

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,13 +1,15 @@
 import React, { Suspense } from "react";
-import { createRoot } from "react-dom/client";
-import i18n from "I18n/i18n.config";
+import ReactDOM from "react-dom/client";
+import i18n from "@localization/i18n.config";
 import { I18nextProvider } from "react-i18next";
-import Root from "Core/root";
-import { store, history } from "Redux/store/store";
+import Root from "@renderer/core/root";
+import { store, history } from "@renderer/redux/store/store";
 import "bulma/css/bulma.css";
 
-const container = document.getElementById("target");
-const root = createRoot(container);
+const root = ReactDOM.createRoot(
+  document.getElementById('target') as HTMLElement
+);
+
 root.render(
   <I18nextProvider i18n={i18n}>
     <Suspense fallback="loading">
